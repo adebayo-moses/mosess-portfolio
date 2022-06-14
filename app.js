efd51f4c6cdedc4c6649c4c1c86c8de57679425e
Adebayo Moses <adebayomoses597@gmail.com>
2022-06-14 22:02:53 +0100

    grid items functions

--- app.js
+++ app.js
@@ -7,4 +7,18 @@ const gridEvents = ["load", "resize"];
 
 function resizeGridItem(item) {
   const rowHeight = 8; //refer to grid-auto-rows data attribute of grid in html
+  const gridItem = item.getElementsByClassName("grid-item-content")[0];
+  let gridItemHeight, rowSpan;
+
+  if (CSSTypedOM) {
+    gridItemHeight;=gridItem.getBoundingClientRect().height;
+    rowSpan = Math.ceil(gridItemHeight / rowHeight);
+    item.attributeStyleMap.set('grid-row-end', `span ${rowSpan}`)
+  } else {
+    gridItemHeight = gridItem.getBoundingClientRect().height;
+    rowSpan = Math.ceil(gridItemHeight / rowHeight);
+    item.style.gridRowEnd = `span ${rowSpan}`;
+  }
 }
+
+function 
