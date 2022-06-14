288285e0e752caff8aa5ee97b08438f4f1dfde1f
Adebayo Moses <adebayomoses597@gmail.com>
2022-06-14 21:57:30 +0100

    init js (grid)

---
+++ app.js
@@ -0,0 +1,10 @@
+const CSSTypedOM = window.CSS && CSS.number ? true : false;
+
+// GRID
+
+const gridtems = document.getElementsByClassName("grid-item");
+const gridEvents = ["load", "resize"];
+
+function resizeGridItem(item) {
+  const rowHeight = 8; //refer to grid-auto-rows data attribute of grid in html
+}
